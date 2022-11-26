b0b94bb4732d598da9c2314e042e666cfbd235d1
alkimgokcen <alkim.gokcen@outlook.com>
2022-11-26 23:56:00 +0300

    Saturation is added for integrated error signal and computed control signal.

--- pid_controller.py
+++ pid_controller.py
@@ -16,12 +16,15 @@ class PID_Controller:
     # pidController.I, pidController.D, pidController.ID 
     #-------------------------------------------------------------------------------------
     #                               ALGORITHM
-    # This algorithm solves the integral and derivative problems by employing
-    # "Euler Method", and considers the error signal for current state.
-    # e[n] = r[n] - y[n], where e is the tracking error, r is the reference
-    # y is the plant output and n is the discrete time.
-    # e[n] is considered to compute control signal u[n].
-    # User may achieve y[n+1] by applying the u[n] to the plant.
+    # - This algorithm solves the integral and derivative problems by employing
+    #   "Euler Method", and considers the error signal for current state.
+    # - e[n] = r[n] - y[n], where e is the tracking error, r is the reference
+    #   y is the plant output and n is the discrete time.
+    #   e[n] is considered to compute control signal u[n].
+    #   User may achieve y[n+1] by applying the u[n] to the plant.
+    # - integralClamp defines the min and max limits of integrated error.
+    #   integral of error is thresholded if the integrated error exceeds these limits
+    # - limit_output defines the saturation boundaries of the control signals.
     #-------------------------------------------------------------------------------------
     #                    Important Notes on Usage of The PID Controller
     # Please, do not forget to call controller algorithm appropriate to controller gains
@@ -37,9 +40,37 @@ class PID_Controller:
         self.sampleTime = sampleTime
         self.delayedTrackingError = 0
         self.integralError = 0
-    
+        self.output_min = None
+        self.output_max = None
+        self.integral_min = None
+        self.integral_max = None
+        
     def getLibVersion(self):
         return self.Version
+    
+    def integralClamp(self, min, max):
+        self.integral_min = min
+        self.integral_max = max
+    
+    def integralCampler(self, integratedValue):
+        if self.integral_min is not None and integratedValue < self.integral_min:
+            return self.integral_min
+        elif self.integral_max is not None and integratedValue > self.integral_max:
+            return self.integral_max
+        else:
+            return integratedValue
+        
+    def limitOutput(self, min, max):
+        self.min = min
+        self.max = max
+        
+    def apply_limits(self, controlSignal):
+        if self.min is not None and controlSignal < self.min:
+            return self.min
+        elif self.max is not None and controlSignal > self.max:
+            return self.max
+        else:
+            return controlSignal 
         
     def computeError(self, actualOutput, desiredOutput):
         trackingError = desiredOutput - actualOutput
@@ -55,50 +86,57 @@ class PID_Controller:
         self.integralError = self.integralError + trackingError * self.sampleTime
         return self.integralError
     
+    
     def PID(self, PlantOutput, reference):
         errorSignal = self.computeError(PlantOutput, reference)
         dError = self.computeDerivative(errorSignal)
         iError = self.computeIntegral(errorSignal)
+        iError = self.integralCampler(iError)
         u = errorSignal * self.Kp + dError * self.Kd + iError * self.Ki
+        u = self.apply_limits(u)
         return u, errorSignal, dError, iError
     
     def P(self, PlantOutput, reference):
         errorSignal = self.computeError(PlantOutput, reference)
-        u = errorSignal * self.Kp 
+        u = errorSignal * self.Kp
+        u = self.apply_limits(u)
         return u, errorSignal
     
     def I(self, PlantOutput, reference):
         errorSignal = self.computeError(PlantOutput, reference)
         iError = self.computeIntegral(errorSignal)
-        u = errorSignal * self.Kp + iError * self.Ki
+        iError = self.integralCampler(iError)
+        u = iError * self.Ki
+        u = self.apply_limits(u)
         return u, errorSignal, iError
     
     def D(self, PlantOutput, reference):
         errorSignal = self.computeError(PlantOutput, reference)
         dError = self.computeDerivative(errorSignal)
         u = self.Kd * dError
+        u = self.apply_limits(u)
         return u, errorSignal, dError
     
     def PI(self, PlantOutput, reference):
         errorSignal = self.computeError(PlantOutput, reference)
         iError = self.computeIntegral(errorSignal)
+        iError = self.integralCampler(iError)
         u = self.Kp * errorSignal + self.Ki * iError
+        u = self.apply_limits(u)
         return u, errorSignal, iError
     
     def PD (self, PlantOutput, reference):
         errorSignal = self.computeError(PlantOutput, reference)
         dError = self.computeDerivative(errorSignal)
         u = self.Kp * dError + self.Kd * dError
+        u = self.apply_limits(u)
         return u, errorSignal, dError
     
     def ID(self, PlantOutput, reference):
         errorSignal = self.computeError(PlantOutput, reference)
         dError = self.computeDerivative(errorSignal)
         iError = self.computeIntegral(errorSignal)
+        iError = self.integralCampler(iError)
         u = self.Kd * dError + self.Ki * iError
+        u = self.apply_limits(u)
         return u, errorSignal, dError, iError
-    
-    
-    
-
-        
